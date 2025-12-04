0e1c975333641af1a4ff9dabf94c9c0666248d5c
Aaron Banister <ab.sitedev@gmail.com>
2025-12-03 20:29:43 -0600

    netlify-react-router support

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import netlifyReactRouter from "@netlify/vite-plugin-react-router";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), netlifyReactRouter()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
